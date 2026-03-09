0c9cbf44d7e034567e69b99744760d37091d5a23
siloricity <83559941+siloricity@users.noreply.github.com>
2026-03-09 13:52:06 -0500

    ghost view

--- ghost.gd
+++ ghost.gd
@@ -1,17 +1,18 @@
 extends RigidBody2D
 @export var sling_multiplier:float
 @export var max_sling:int
-var released := false
-# rotate the sling
-func _process(_delta):
+var vel: Vector2
+
+func _ready():
 	var mouse = get_global_mouse_position()
-	var vel: Vector2 = sling_multiplier*(self.global_position-mouse)
-	if Input.is_action_pressed("click"):
-		self.look_at(mouse)
-		self.rotation_degrees -= 90
-		self.linear_velocity = Vector2.ZERO
-	if Input.is_action_just_released("click"):
-		self.freeze = false
-		self.gravity_scale = 0
-		self.linear_velocity = vel
-		self.gravity_scale = 1
+	self.look_at(mouse)
+	self.rotation_degrees -= 90
+	vel = sling_multiplier*(self.global_position-mouse)
+	self.linear_velocity = vel.limit_length(max_sling)
+	var tween = get_tree().create_tween()
+	tween.tween_property(self,"modulate:a",0, 1.2)
+	tween.tween_callback(func(): queue_free())
+func _on_body_entered(body: Node) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self,"modulate:a",0, 0.4)
+	tween.tween_callback(func(): queue_free())
--- testarea.gd
+++ testarea.gd
@@ -1,3 +1,15 @@
 extends Node2D
-func _ready():
-	print($bobby.ghost)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("click"):
+		if $bobby.dead == true: pass
+		else:
+			$Timer.start()
+	if Input.is_action_just_released("click"):
+		$Timer.stop()
+
+
+func _on_timer_timeout() -> void:
+	var ghost = $bobby.ghost.instantiate()
+	ghost.position = $bobby.position
+	add_child(ghost)
