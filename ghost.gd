5d62d2b906f41b2b9e4a11f9535166987061262f
siloricity <83559941+siloricity@users.noreply.github.com>
2026-03-14 14:03:21 -0500

    apply slip to ghost

--- ghost.gd
+++ ghost.gd
@@ -17,3 +17,14 @@ func _on_body_entered(_body: Node) -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(self,"modulate:a",0, 0.4)
 	tween.tween_callback(func(): queue_free())
+# Physics anti-softlock by sunshinecoco12 on Discord
+func _physics_process(delta: float) -> void:
+	var collision_info: KinematicCollision2D = move_and_collide(linear_velocity * delta, true)
+	var ball_radius: float = 15.0
+	var friction: float = 0.3
+	if collision_info:
+		if collision_info.get_collider().has_method("method"):
+			self.physics_material_override.bounce = 0
+		else:
+			self.physics_material_override.bounce = 0.99
+		angular_velocity = -linear_velocity.dot(collision_info.get_normal().orthogonal()) / ball_radius * friction
