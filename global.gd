63b16eff4b8f4db3ce32c83ffb68c7e02b21ff5c
siloricity <83559941+siloricity@users.noreply.github.com>
2026-03-14 13:59:14 -0500

    ?

--- global.gd
+++ global.gd
@@ -1,11 +1,15 @@
 extends Node
+@onready var quack = preload("res://mystery.tscn")
 func _input(_InputEvent):
 	if Input.is_action_just_pressed("esc"):
 		get_tree().change_scene_to_file("res://title.tscn")
 	if Input.is_action_just_pressed("r"):
 		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("g"):
+		add_child(quack.instantiate())
 func _ready():
 	# Application ID
 	DiscordRPC.app_id = 1482445160360050882
 	DiscordRPC.details = "boing"
 	DiscordRPC.refresh()
+	
